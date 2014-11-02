17ea5d2419e2a8a7350135e820e0525cbd91d260
bliti <pablo.rivera.programmer@gmail.com>
2014-11-02 19:04:18 -0400

    updated blog url

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ try:
     from development_settings import URLS
 except ImportError:
     URLS = {
-            'blog': 'http://wutbot.tumblr.com/',
+            'blog': 'http://blog.wutbot.com',
             'twitter': 'https://twitter.com/wutbot5000',
             'youtube': 'https://www.youtube.com/channel/UCOlSfiGQCT5_rmcxQDNygkg',
             'store': 'http://tindie.com' 
