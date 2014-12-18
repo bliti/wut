88f2b6d290053fa1ad94c0f1db895cd9688c4752
bliti <pablo.rivera.programmer@gmail.com>
2014-12-17 21:42:36 -0400

    updating templates

--- app.py
+++ app.py
@@ -11,24 +11,24 @@ def index():
     return render_template('under_construction.html')
 
 
-@app.route('/blog', methods=['GET'])
-def blog():
-    return redirect(URLS['blog'])
+#@app.route('/blog', methods=['GET'])
+#def blog():
+#    return redirect(URLS['blog'])
 
 
-@app.route('/twitter', methods=['GET'])
-def twitter():
-    return redirect(URLS['twitter'])
+#@app.route('/twitter', methods=['GET'])
+#def twitter():
+#    return redirect(URLS['twitter'])
 
 
-@app.route('/youtube', methods=['GET'])
-def youtube():
-    return redirect(URLS['youtube'])
+#@app.route('/youtube', methods=['GET'])
+#def youtube():
+#    return redirect(URLS['youtube'])
 
 
-@app.route('/store', methods=['GET'])
-def store():
-    return redirect(URLS['store'])
+#@app.route('/store', methods=['GET'])
+#def store():
+#    return redirect(URLS['store'])
 
 
 if __name__ == '__main__':
